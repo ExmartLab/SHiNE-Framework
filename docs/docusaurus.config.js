9eb6fe91b1af2c16c941b50f05442192f7d46585
Simon Scholz <sschol24@smail.uni-koeln.de>
2025-09-25 10:44:41 +0200

    docs: deploy to gh

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,15 +15,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://exmartlab.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/build/',
+  baseUrl: '/SHiNE-Framework/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ExmartLab', // Usually your GitHub org/user name.
+  projectName: 'SHiNE-Framework', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
